a3e78835225841bbcd2f77c0200d137072884245
JigarRajput <rajputjigar455@gmail.com>
2022-08-20 11:40:43 +0530

    changed card styling a bit

--- src/components/ProductCard.js
+++ src/components/ProductCard.js
@@ -32,14 +32,14 @@ export default function ProductCard({ books }) {
 
             {books.map((book) => (
                 <div className="card rounded-4 shadow-lg bg-light">
-                    <img src={book.image} className='rounded-top ' alt="Denim Jeans" />
+                    <img src={book.image} className='rounded-4 p-3' alt="Denim Jeans" />
 
                     <div className='card-body'>
-                        <h4 className='card-title text-dark'><strong>{book.title}</strong></h4>
-                        <p className='card-text text-dark'>{book.description}</p>
-                        <div className='price-container text-dark'><p>MRP <del>{book.price}</del> <p className='discount'>{book.discount} % OFF</p></p></div>
-                        <p className='net-price text-dark'><strong>Net price: &nbsp; {Math.round(book.price - book.price * book.discount / 100)}</strong></p>
-                        <button className="btn btn-danger btn-lg rounded-3 mb-0" onClick={() => { addToCartHandler(book); console.log("updated state", store.getState()) }} >Add to Cart</button>
+                        <h6 className='card-title text-dark text-center'><strong>{book.title}</strong></h6>
+                        {/* <p className='card-text text-dark'>{book.description}</p> */}
+                        <div className='price-container text-dark d-flex justify-content-center py-3'><p>MRP: &nbsp; <del>{book.price}</del> <p className='discount'>{book.discount} % &nbsp; OFF</p></p></div>
+                        <p className='net-price text-dark text-center'><strong>Net price: &nbsp; {Math.round(book.price - book.price * book.discount / 100)}</strong></p>
+                        <button className="btn btn-secondary btn-lg rounded-3 mb-0" onClick={() => { addToCartHandler(book); console.log("updated state", store.getState()) }} >Add to Cart</button>
                     </div>
                 </div>
             ))
